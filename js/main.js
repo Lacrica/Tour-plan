1f21bfab3d113bec6bf71de6a5b6410d32d34a82
Lacrica <Lacricachan@gmail.com>
2020-09-24 02:55:44 +0300

    Create: added yandex maps. booking info

--- js/main.js
+++ js/main.js
@@ -12,3 +12,47 @@ var mySwiper = new Swiper(".swiper-container", {
     onlyInViewport: false,
   },
 })
+ymaps.ready(init);
+
+var myMap;
+
+function init () {
+    // Параметры карты можно задать в конструкторе.
+    myMap = new ymaps.Map(
+        // ID DOM-элемента, в который будет добавлена карта.
+        'map',
+        // Параметры карты.
+        {
+            // Географические координаты центра отображаемой карты.
+            center: [18.46, -66.08],
+            // Масштаб.
+            zoom: 15,
+            // Тип покрытия карты: "Спутник".
+            type: 'yandex#satellite'
+        }, {
+            // Поиск по организациям.
+            searchControlProvider: 'yandex#search'
+        }
+    );
+}
+
+function setCenter () {
+    myMap.setCenter([18.463621, -66.086021]);
+}
+
+function setBounds () {
+    // Bounds - границы видимой области карты.
+    // Задаются в географических координатах самой юго-восточной и самой северо-западной точек видимой области.
+    myMap.setBounds([[37, 38], [39, 40]]);
+}
+
+function setTypeAndPan () {
+    // Меняем тип карты на "Гибрид".
+    myMap.setType('yandex#hybrid');
+    // Плавное перемещение центра карты в точку с новыми координатами.
+    myMap.panTo([62.915, 34.461], {
+            // Задержка между перемещениями.
+            delay: 1500
+        });
+        
+}
