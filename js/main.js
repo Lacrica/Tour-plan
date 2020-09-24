b45c9e6aa2041e6f7f400db126acd17b9ecdce6e
Lacrica <Lacricachan@gmail.com>
2020-09-24 03:49:13 +0300

    fix: ymaps , added placemark

--- js/main.js
+++ js/main.js
@@ -12,47 +12,18 @@ var mySwiper = new Swiper(".swiper-container", {
     onlyInViewport: false,
   },
 })
+
 ymaps.ready(init);
 
-var myMap;
-
-function init () {
-    // Параметры карты можно задать в конструкторе.
-    myMap = new ymaps.Map(
-        // ID DOM-элемента, в который будет добавлена карта.
-        'map',
-        // Параметры карты.
-        {
-            // Географические координаты центра отображаемой карты.
-            center: [18.46, -66.08],
-            // Масштаб.
-            zoom: 15,
-            // Тип покрытия карты: "Спутник".
-            type: 'yandex#satellite'
+function init() {
+    var myMap = new ymaps.Map('map', {
+            center: [-20.308415, 57.367221],
+            zoom: 18
         }, {
-            // Поиск по организациям.
             searchControlProvider: 'yandex#search'
-        }
-    );
-}
+        }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter());
 
-function setCenter () {
-    myMap.setCenter([18.463621, -66.086021]);
-}
+    myMap.geoObjects.add(myPlacemark);
 
-function setBounds () {
-    // Bounds - границы видимой области карты.
-    // Задаются в географических координатах самой юго-восточной и самой северо-западной точек видимой области.
-    myMap.setBounds([[37, 38], [39, 40]]);
-}
-
-function setTypeAndPan () {
-    // Меняем тип карты на "Гибрид".
-    myMap.setType('yandex#hybrid');
-    // Плавное перемещение центра карты в точку с новыми координатами.
-    myMap.panTo([62.915, 34.461], {
-            // Задержка между перемещениями.
-            delay: 1500
-        });
-        
 }
