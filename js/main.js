4a09a8deafe23b66a9bbc66dedb400e9627e732c
Lacrica <Lacricachan@gmail.com>
2020-09-25 16:23:16 +0300

    Create: reviews section & slider.Fix: hotel-slider

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-var mySwiper = new Swiper(".swiper-container", {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    prevEl: ".hotel-slider__button--prev",
+    nextEl: ".hotel-slider__button--next",
   },
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
-})
+});
+var reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    prevEl: ".reviews-slider__button--prev",
+    nextEl: ".reviews-slider__button--next",
+  },
+});
 
 ymaps.ready(init);
 
